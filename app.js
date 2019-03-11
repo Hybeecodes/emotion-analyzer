1d88520f4376326b3517de905854ca603431fa2a
Hybeecodes <obikoya11@gmail.com>
2019-03-12 00:57:53 +0100

    changed port

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/analyse',(req,res,next) => {
 });
 
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`server running @ PORT ${PORT}`);
 })
