67a09c863e169d3d23b095fb62107960fdb4f6e5
Hybeecodes <obikoya11@gmail.com>
2019-03-12 00:47:03 +0100

    added more training data

--- src/training-data/d-happy.js
+++ src/training-data/d-happy.js
@@ -12,7 +12,52 @@ const happy = [
       {
         input: "What a good day!",
         output: {happy: 1}
-      }
+      },
+      {
+        input: "Happiness is my birthright. I embrace happiness as my setpoint state of being",
+        output: {happy: 1}
+      },
+      {
+        input: "I feel joy and contentment at this moment right now",
+        output: {happy: 1}
+      },
+      {
+        input: "I awaken in the morning feeling happy and enthusiastic about life",
+        output: {happy: 1}
+      },
+      {
+        input: "I can tap into a wellspring of inner happiness anytime I wish",
+        output: {happy: 1}
+      },
+      {
+        input: "By allowing myself to be happy, I inspire others to be happy as well",
+        output: {happy: 1}
+      },
+      {
+        input: "I have fun with all of my endeavors, even the most mundane",
+        output: {happy: 1}
+      },
+      {
+        input: "I look at the world around me and can't help but smile and feel joy",
+        output: {happy: 1}
+      },
+      {
+        input: "I find joy and pleasure in the most simple things in life",
+        output: {happy: 1}
+      },
+      {
+        input: "I have an active sense of humor and love to share laughter with others",
+        output: {happy: 1}
+      },
+
+      {
+        input: "My heart is overflowing with joy",
+        output: {happy: 1}
+      },
+      {
+        input: "I rest in happiness when I go to sleep, knowing all is well in my world",
+        output: {happy: 1}
+      },
 
 ];
 
